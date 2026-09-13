a7d75e969ab04050bb003c8a7c7947bce18d353b
Dylan McCune <dylan@dylanmccune.com>
2026-09-13 13:58:17 -0700

    Got wifi working

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,28 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include "secrets.h"
 
 int sensorPin = A0;
 int val = 0;
 
 void setup() {
     Serial.begin(115200);
+    delay(2000);
     pinMode(sensorPin, INPUT);
     analogSetAttenuation(ADC_11db);
+    
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    Serial.print("Wifi connecting");
+    while (WiFi.status() != WL_CONNECTED) {
+         Serial.print('.');
+         delay(500);
+    }
+    Serial.println();
+
+    Serial.println("Connected!");
+
+    
 }
 
 void loop() {
